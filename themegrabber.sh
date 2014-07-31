6dcc093a5c8db6fffd086f8f378d3f972a36c7d5
David Barron <dbarron@hostgator.com>
2014-07-31 09:02:48 -0600

    ThemeGrabber is finished

--- themegrabber.sh
+++ themegrabber.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-for file in /home/hgdesign/www/themes/WordPress/*
 number=1
+choice=""
+themes[0]=""
+for file in /home/hgdesign/www/themes/WordPress/* 
 do
-	echo "$number. $file"
+	echo "$number. ${file:36}"
+	(( number++ ))
+	themes[${number-1}]=${file:36}
 done
+
+echo -e "Which theme would you like?"
+read choice
+echo "You chose ${themes[$choice+1]}"
